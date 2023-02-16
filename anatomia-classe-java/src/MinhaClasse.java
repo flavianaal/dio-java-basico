905f3df13273d3cec2c4a5ba67f8056df607d2fe
Flaviana Andrade <flavianaandradedelima@gmail.com>
2023-02-16 17:05:04 -0300

    Anatomia das classes

---
+++ anatomia-classe-java/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+public class MinhaClasse {
+
+    /*
+     * por padrao todas as primeiras letras dos nomes das classes comecaram com
+     * letras maisculas
+     */
+
+    public static void main(String[] args) {
+
+        System.out.print("Minha inicialização");
+    }
+
+}
