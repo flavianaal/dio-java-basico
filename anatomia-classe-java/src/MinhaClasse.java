33267bfae65100d86c3c232cba16aa504eb1ea8c
Flaviana Andrade <flavianaandradedelima@gmail.com>
2023-03-02 10:15:59 -0300

    atualizaçao

--- anatomia-classe-java/src/MinhaClasse.java
+++ anatomia-classe-java/src/MinhaClasse.java
@@ -7,7 +7,18 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
 
-        System.out.print("Minha inicialização");
+        String primeiroNome = "Flaviana";
+        String segundoNome = "Andrade";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
---
+++ anatomia-classe-java/src/edu/flaviana/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,17 @@
+package edu.flaviana.primeirasemana;
+import javax.lang.model.util.ElementScanner6;
+
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if (mediaFinal < 6)
+
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+
+}
